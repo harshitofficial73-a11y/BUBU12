ce45ea65394b73db4b5ae8452b3a28cd61b2725e
harshitofficial73-a11y <harshitofficial73@gmail.com>
2026-08-22 13:51:43 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,9 @@
-// Copy this file to supabase-config.js and replace both placeholders.
-window.BUBU_SUPABASE_URL = "https://YOUR_PROJECT.supabase.co";
-window.BUBU_SUPABASE_ANON_KEY = "YOUR_PUBLISHABLE_OR_ANON_KEY";
-window.BUBU_ALLOW_UNIVERSAL_OTP = true; // development only
+window.BUBU_SUPABASE_URL =
+  "https://wiltfiunyftyyqmwrrdt.supabase.co";
+
+window.BUBU_SUPABASE_ANON_KEY =
+  "sb_publishable_5rGM-0LeMsOVdw827u006A_fer0Ltqi";
+
+window.BUBU_ALLOW_UNIVERSAL_OTP = true;
 window.BUBU_UNIVERSAL_OTP = "079757";
 window.BUBU_SEED_PASSWORD = "Bubu@2026";
