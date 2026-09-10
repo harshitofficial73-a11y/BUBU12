dcfc402cab2ed41664b18ef5257282980a6af97c
harshitofficial73-a11y <harshitofficial73@gmail.com>
2026-09-10 15:42:54 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -4,6 +4,6 @@ window.BUBU_SUPABASE_URL =
 window.BUBU_SUPABASE_ANON_KEY =
   "sb_publishable_5rGM-0LeMsOVdw827u006A_fer0Ltqi";
 
-window.BUBU_ALLOW_UNIVERSAL_OTP = true;
+window.BUBU_ALLOW_UNIVERSAL_OTP = false;
 window.BUBU_UNIVERSAL_OTP = "079757";
 window.BUBU_SEED_PASSWORD = "Bubu@2026";
